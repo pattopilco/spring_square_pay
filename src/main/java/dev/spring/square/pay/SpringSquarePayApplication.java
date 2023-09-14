fa98cb63fcd5868119617c345e04a77e6c321e6c
Patricio Pilco <ppilco@galavail.com>
2023-09-14 10:12:22 -0500

    create project spring square pay

---
+++ src/main/java/dev/spring/square/pay/SpringSquarePayApplication.java
@@ -0,0 +1,13 @@
+package dev.spring.square.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSquarePayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSquarePayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/spring/square/pay/SpringSquarePayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.spring.square.pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSquarePayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
